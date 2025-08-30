76686ef79c85646ac38fe781ad12144c9a81182e
Chiklit Gohil <139043691+chiklitgohil@users.noreply.github.com>
2025-08-30 12:30:31 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Path to your service account key JSON file
-cred = credentials.Certificate("D:\dev\code\hackout25\projectM\projectm-84c11-firebase-adminsdk-fbsvc-bfcbf9e50d.json")
+cred = credentials.Certificate("projectm-84c11-firebase-adminsdk-fbsvc-bfcbf9e50d.json")
 firebase_admin.initialize_app(cred)
 
 # Get Firestore client
