d956aed1201a6c601cb6bb18b13f6b9e7291c630
Chiklit Gohil <139043691+chiklitgohil@users.noreply.github.com>
2025-08-30 12:26:33 +0530

    database

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,24 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase_key.json")  # your downloaded key
+# Path to your service account key JSON file
+cred = credentials.Certificate("D:\dev\code\hackout25\projectM\projectm-84c11-firebase-adminsdk-fbsvc-bfcbf9e50d.json")
 firebase_admin.initialize_app(cred)
 
-db = firestore.client()  # Firestore client
+# Get Firestore client
+db = firestore.client()
+
+# Add data
+doc_ref = db.collection("users").document("chiklit")
+doc_ref.set({
+    "name": "Chiklit Gohil",
+    "age": 18,
+    "isStudent": True
+})
+
+# Read data
+doc = db.collection("users").document("chiklit").get()
+if doc.exists:
+    print("Document data:", doc.to_dict())
+else:
+    print("No such document!")
